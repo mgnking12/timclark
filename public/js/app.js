37a0c270c21d1936830827f73e94aa47c9959e26
Megan King <mgnking12@gmail.com>
2016-08-18 11:02:06 -0500

    +_+

---
+++ public/js/app.js
@@ -0,0 +1,42 @@
+  (function($) {
+      $(function() {
+          // Hide Header on on scroll down
+          var didScroll;
+          var lastScrollTop = 0;
+          var delta = 1;
+          var navbarHeight = $('header').outerHeight();
+
+          $(window).scroll(function(event) {
+              didScroll = true;
+          });
+
+          setInterval(function() {
+              if (didScroll) {
+                  hasScrolled();
+                  didScroll = false;
+              }
+          }, 250);
+
+          function hasScrolled() {
+              var st = $(this).scrollTop();
+
+              // Make sure they scroll more than delta
+              if (Math.abs(lastScrollTop - st) <= delta)
+                  return;
+
+              // If they scrolled down and are past the navbar, add class .nav-up.
+              // This is necessary so you never see what is "behind" the navbar.
+              if (st > lastScrollTop && st > navbarHeight) {
+                  // Scroll Down
+                  $('header').removeClass('nav-down').addClass('nav-up');
+              } else {
+                  // Scroll Up
+                  if (st + $(window).height() < $(document).height()) {
+                      $('header').removeClass('nav-up').addClass('nav-down');
+                  }
+              }
+
+              lastScrollTop = st;
+          }
+      }); // end of document ready
+  })(jQuery); // end of jQuery name space
