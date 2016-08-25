0dad1961c9aeee8c33c99bfdde1c232efca259ae
Megan King <mgnking12@gmail.com>
2016-08-25 15:17:15 -0500

    tables

--- app.js
+++ app.js
@@ -8,12 +8,36 @@ var works = [{
     Link: '/1',
     Text: 'Stairs',
     Photo: [
-        '/photos/IMG_0029.jpg',
-        '/photos/IMG_0030.jpg',
-        '/photos/IMG_0031.jpg',
-        '/photos/IMG_0032.jpg'
+        '/photos/stairs/IMG_0029.jpg',
+        '/photos/stairs/IMG_0030.jpg',
+        '/photos/stairs/IMG_0031.jpg',
+        '/photos/stairs/IMG_0032.jpg',
+        '/photos/stairs/IMG_3422.jpg'
     ],
-    Background: '/photos/IMG_0030.jpg',
+    Background: '/photos/stairs/IMG_0030.jpg',
+    About: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.'
+}, {
+    Link: '/2',
+    Text: 'Tables',
+    Photo: [
+        '/photos/tables/end_table1.jpg',
+        '/photos/tables/end_table2.jpg',
+        '/photos/tables/end_table3.jpg',
+        '/photos/tables/end_table4.jpg',
+        '/photos/tables/end_table5.jpg',
+        '/photos/tables/kid_table1.jpg',
+        '/photos/tables/kid_table2.jpg',
+        '/photos/tables/side_table1.jpg',
+        '/photos/tables/side_table2.jpg',
+        '/photos/tables/side_table3.jpg',
+        '/photos/tables/table_progress1.jpg',
+        '/photos/tables/table_progress2.jpg',
+        '/photos/tables/table_progress3.jpg',
+        '/photos/tables/table_progress4.jpg',
+        '/photos/tables/table_progress5.jpg',
+        '/photos/tables/tv_stand.jpg',
+    ],
+    Background: '/photos/tables/end_table2.jpg',
     About: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.'
 }];
 var nav = [{
